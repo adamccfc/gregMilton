d8bba058dfc9c8f8644505df4b43cef5ab36bd94
amorgan <adam.morgan013@gov.wales>
2018-09-11 13:40:07 +0100

    Added grid styles & card styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,22 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="container">
+      <div className="row">
+        <div class="col-12">
+          <h1>Hi people</h1>
+          <p>Welcome to your new Gatsby site.</p>
+          <p>Now go build something great.</p>
+          <Link to="/page-2/">Go to page 2</Link>
+        </div>
+        <div className="col-6">
+          <div className="card">
+            <div className="card__tag">Stonework</div>
+            <h3 className="card__headline">This is where the headline goes</h3>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
