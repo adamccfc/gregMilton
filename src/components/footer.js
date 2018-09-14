2804dd81e42a6c678a39fc67cc1de34a4b54983b
amorgan <adam.morgan013@gov.wales>
2018-09-14 15:23:51 +0100

    Add social links to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,13 +4,17 @@ const Footer = ({ siteTitle }) => (
   <footer className="footer">
     <div className="container">
       <div className="row">
-        <div className="col-12 footer__divider">
-          <h5 className="footer__title">Contact Me</h5>
+        <div className="col-4">
+          <h5 className="footer__title">Contact</h5>
           <a className="footer__link" href="tel:0144347223">01443 47223</a>
           <a className="footer__link" href="tel:07711686833">07711 686833</a>
           <a className="footer__link" href="mailto:greghmilton@hotmail.co.uk?Subject=Hello%Greg">greghmilton@hotmail.co.uk</a>
         </div>
-        <div className="col-12">
+        <div className="col-4">
+          <h5 className="footer__title">Social</h5>
+          <a className="footer__link" href="#">Facebook</a>
+        </div>
+        <div className="col-12 footer__divider">
           <p>Designed and developed by <a href="http:www.adammorgan.wales" target="_blank" rel="noopener noreferrer">Adam Morgan</a>.</p>
         </div>
       </div>
